78ee39f1f47d0d5c104d85efabef17bdd153cd0f
Andriy Alieksieienko <andriy.alieksieienko@gmail.com>
2022-04-23 21:14:34 +0300

    updated Orc's atack method

--- src/Orc.js
+++ src/Orc.js
@@ -3,14 +3,16 @@ class Orc {
     this.name = name;
     this.weapon = 'axe';
     this.health = 125;
+    this.defeat = 10;
   }
 
   say() {
     console.log(`I am orc ${this.name}`);
   }
 
-  atack() {
-    console.log(`${this.name} used ${this.weapon}`);
+  atack(person) {
+    console.log(`${this.name} atacked ${person}`);    
+    person.health - this.defeat;
   }
 
   defense() {
@@ -18,6 +20,6 @@ class Orc {
   }
 
   angry() {
-
+    this.defeat *= 2;
   }
 }
